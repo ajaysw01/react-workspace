a045d91e07822143089a45f1880926627267af8b
Ajay Wankhade <ajaysw45@gmail.com>
2025-02-13 17:40:52 +0530

    react hook forms

---
+++ 11_reactHookForm/src/App.jsx
@@ -0,0 +1,73 @@
+import { useForm } from "react-hook-form";
+
+import "./App.css";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  async function onSubmit(data) {
+    //API call simulate
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("form submiting", data);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>First Name:</label>
+          <input
+            className={errors.firstName ? "input-error" : ""}
+            {...register("firstName", {
+              required: true,
+              minLength: { value: 3, message: "min len should be 3" },
+              maxLength: { value: 10, message: "max len should be 10" },
+            })}
+          />
+          {errors.firstName && (
+            <p className="error-msg">{errors.firstName.message}</p>
+          )}
+        </div>
+        <br />
+
+        <div>
+          <label>Middle Name:</label>
+          <input
+            className={errors.middleName ? "input-error" : ""}
+            {...register("middleName")}
+          />
+        </div>
+        <br />
+
+        <div>
+          <label>Last Name:</label>
+          <input
+            className={errors.lastName ? "input-error" : ""}
+            {...register("lastName", {
+              pattern: {
+                value: /^[A-Za-z]+$/i,
+                message: "last name is not as per the rules",
+              },
+            })}
+          />
+          {errors.lastName && (
+            <p className="error-msg">{errors.lastName.message}</p>
+          )}
+        </div>
+        <br />
+        <input
+          type="submit"
+          disabled={isSubmitting}
+          value={isSubmitting ? "Submitting" : "Submit"}
+        />
+      </form>
+    </>
+  );
+}
+
+export default App;
